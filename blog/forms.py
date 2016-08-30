dcef754ea32b4813e781ee7e13ff63e7972d6f45
TianchenMa <Mike@vlan-2646-10-19-125-148.student.wireless.sydney.edu.au>
2016-08-30 16:35:54 +1000

    View modify

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,12 @@
 from django import forms
 
 
+class BlogForm(forms.Form):
+    title = forms.CharField(max_length=100)
+    content = forms.CharField(widget=forms.Textarea)
+    private = forms.CharField()
+
+
 class CommentForm(forms.Form):
     author_id = forms.CharField()
     content = forms.CharField(widget=forms.Textarea)
